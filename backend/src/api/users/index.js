c76673826acb1a492089d23ed1398c2d4a8b2770
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2018-03-06 13:41:53 +0100

    Update APIDoc

--- backend/src/api/users/index.js
+++ backend/src/api/users/index.js
@@ -3,8 +3,43 @@ const { Router } = require('express');
 const router = new Router();
 const controler = require('./users.controller');
 
+/**
+ * @api {get} / Get all users
+ * @apiName GetAllUsers
+ * @apiGroup Static Pages
+ * @apiDescription Affiche tous les utilisateurs
+ */
 router.get('/', controler.findAll);
-// router.get('/:id', controler.findOne);
-// router.post('/', controler.create);
+
+/**
+ * @api {post} / Create a user
+ * @apiName CreateUser
+ * @apiGroup Static Pages
+ * @apiDescription Crée un utilisateur
+ *
+ * @apiParam  {String} name Nom de l'user à créer
+ * @apiParam  {String} role Rôle de l'user à créer ("teacher" ou "student")
+ */
+router.post('/', controler.create);
+
+/**
+ * @api {get} / Get one user
+ * @apiName GetUser
+ * @apiGroup Static Pages
+ * @apiDescription Affiche un utilisateur à partir de son id
+ *
+ * @apiParam  {String} id id de l'user à récupérer
+ */
+router.get('/:id', controler.findOne);
+
+/**
+ * @api {get} / Get status of a user
+ * @apiName GetUserStatus
+ * @apiGroup Static Pages
+ * @apiDescription Affiche le statut d'un utilisateur à partir de son id
+ *
+ * @apiParam  {String} id id de l'user à récupérer
+ */
+router.get('/:id/status', controler.getStatus);
 
 module.exports = router;
--- backend/src/api/users/users.controller.js
+++ backend/src/api/users/users.controller.js
@@ -9,30 +9,30 @@ module.exports.findAll = (req, res) => {
   });
 };
 
-// module.exports.findOne = (req, res) => {
-//   User.findOne(
-//     { _id: req.params.id },
-//     (err, user) => {
-//       if (err) return res.status(500).json(err);
-//       return res.status(200).json(user);
-//     },
-//   );
-// };
-//
-// module.exports.getStatus = (req, res) => {
-//   User.findOne(
-//     { _id: req.params.id },
-//     (err, user) => {
-//       if (err) return res.status(500).json(err);
-//       return res.status(200).json(user.status);
-//     },
-//   );
-// };
-//
-// module.exports.create = (req, res) => {
-//   const user = new User(req.body);
-//   user.save((err) => {
-//     if (err) return res.status(500).json(err);
-//     return res.status(201).json(user);
-//   });
-// };
+module.exports.findOne = (req, res) => {
+  User.findOne(
+    { _id: req.params.id },
+    (err, user) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(user);
+    },
+  );
+};
+
+module.exports.getStatus = (req, res) => {
+  User.findOne(
+    { _id: req.params.id },
+    (err, user) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).send(user.status);
+    },
+  );
+};
+
+module.exports.create = (req, res) => {
+  const user = new User(req.body);
+  user.save((err) => {
+    if (err) return res.status(500).json(err);
+    return res.status(201).json(user);
+  });
+};
