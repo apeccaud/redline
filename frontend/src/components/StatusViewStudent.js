21c5bd83bc251bebe9ca1c438f86617b4e0102f4
Lucas Louf <lucaslouf@gmail.com>
2018-03-12 15:30:36 +0100

    Change UI/UX for the StatusViewStudent

--- frontend/src/components/StatusViewStudent.js
+++ frontend/src/components/StatusViewStudent.js
@@ -2,16 +2,38 @@ import React, {PureComponent} from 'react';
 import {withStyles} from 'material-ui';
 import Button from 'material-ui/Button';
 import PropTypes from 'prop-types';
-
+import {Typography} from 'material-ui';
+import {Clear as StopIcon, Done as OkIcon, Info as InfoIcon} from 'material-ui-icons';
 
 const styles = {
-  stopButton: {
-    padding: '14px',
-    margin: '20px',
-  },
-  centerMe: {
-    textAlign: 'center',
-  },
+    centerMe: {
+        textAlign: 'center',
+    },
+    spaceMe: {
+        padding: 20
+    },
+    stopButton: {
+        width: 150,
+        height: 150,
+        padding: 30
+    },
+    stopButtonText: {
+        marginTop: 20,
+        fontSize:20
+    },
+    stopIcon: {
+        width: 60,
+        height: 60,
+    },
+    infoText: {
+        color: '#9c9c9c'
+    },
+    infoIcon: {
+        width: 24,
+        height: 24,
+        verticalAlign: 'bottom',
+        paddingRight: 5
+    }
 };
 
 class StatusViewStudent extends PureComponent {
@@ -29,27 +51,42 @@ class StatusViewStudent extends PureComponent {
 
   render() {
     return (
-      <div>
-
-        <div className={this.props.classes.centerMe}>
-          Bienvenue {this.props.user.name}
-        </div>
-
-        <div className={this.props.classes.centerMe}>
-          <Button
-            variant='raised'
-            color={this.props.user.status === 'lost' ? 'secondary' : 'primary'}
-            className={this.props.classes.stopButton}
-            onClick={this.onPressButton}>
-            {this.props.user.status === 'lost' ? <span>J'ai tout compris</span> : <span>Ça devient compliqué</span>}
-          </Button>
-        </div>
-
         <div>
-          Appuyer sur le bouton permet de notifer au professeur que vous avez du mal à suivre
-        </div>
 
-      </div>
+            <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
+                <Typography variant="headline">
+                    Bienvenue {this.props.user.name}
+                </Typography>
+            </div>
+
+            <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
+                <Button
+                    variant="fab"
+                    color={this.props.user.status === 'lost' ? 'primary' : 'secondary'}
+                    style={styles.stopButton}
+                    onClick={this.onPressButton}>
+                    {this.props.user.status === 'lost' ? <OkIcon style={styles.stopIcon}/> :
+                        <StopIcon style={styles.stopIcon}/>}
+                </Button>
+                <Typography
+                    variant="button"
+                    color={this.props.user.status === 'lost' ? 'primary' : 'secondary'}
+                    className={this.props.classes.stopButtonText}>
+                    {this.props.user.status === 'lost' ? <span>J'ai tout compris</span> :
+                        <span>J'ai des difficultés</span>}
+                </Typography>
+            </div>
+
+            <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
+                <Typography
+                    className={this.props.classes.infoText}
+                    variant="subheading">
+                    <InfoIcon style={styles.infoIcon}/>
+                    Appuyer sur le bouton permet de notifer au professeur que vous avez du mal à suivre
+                </Typography>
+            </div>
+
+        </div>
     )
   }
 }
