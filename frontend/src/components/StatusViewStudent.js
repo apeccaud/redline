52b1e79e2d04827099b9d9d1e600526489579a5a
Lucas Louf <lucaslouf@gmail.com>
2018-03-14 17:04:26 +0100

    Change UI/UX for the StatusViewStudent & StatusViewTeacher

--- frontend/src/components/StatusViewStudent.js
+++ frontend/src/components/StatusViewStudent.js
@@ -1,9 +1,11 @@
 import React, {PureComponent} from 'react';
 import {withStyles} from 'material-ui';
 import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import Tooltip from 'material-ui/Tooltip';
 import PropTypes from 'prop-types';
-import {Typography} from 'material-ui';
 import {Clear as StopIcon, Done as OkIcon, Info as InfoIcon} from 'material-ui-icons';
+/*import {Stop as StopIcon, PlayArrow as OkIcon, Info as InfoIcon} from 'material-ui-icons';*/
 
 const styles = {
     centerMe: {
@@ -17,9 +19,17 @@ const styles = {
         height: 150,
         padding: 30
     },
-    stopButtonText: {
-        marginTop: 20,
-        fontSize:20
+    statusText: {
+        marginTop: 40
+    },
+    statusLabelText: {
+        display: 'inline-block',
+        color: '#9c9c9c'
+    },
+    statusActualText: {
+        fontSize: 20,
+        paddingLeft: 5,
+        display: 'inline-block'
     },
     stopIcon: {
         width: 60,
@@ -37,58 +47,72 @@ const styles = {
 };
 
 class StatusViewStudent extends PureComponent {
-  static propTypes = {
-    classes: PropTypes.object.isRequired,
-    user: PropTypes.object,
-    onClickButton: PropTypes.func,
-  };
+    static propTypes = {
+        classes: PropTypes.object.isRequired,
+        user: PropTypes.object,
+        onClickButton: PropTypes.func,
+    };
 
-  onPressButton = () => {
-    // Change parent state (user)
-    let newStatus = this.props.user.status === 'lost' ? 'neutral' : 'lost';
-    this.props.onClickButton(newStatus);
-  };
+    onPressButton = () => {
+        // Change parent state (user)
+        let newStatus = this.props.user.status === 'lost' ? 'neutral' : 'lost';
+        this.props.onClickButton(newStatus);
+    };
 
-  render() {
-    return (
-        <div>
+    render() {
+        /*const {classes} = this.props;*/
 
-            <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
-                <Typography variant="headline">
-                    Bienvenue {this.props.user.name}
-                </Typography>
+        return (
+            <div>
+
+                <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
+                    <Typography variant="headline">
+                        Bienvenue {this.props.user.name}
+                    </Typography>
+                </div>
+
+                <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
+                    <Tooltip
+                        title={this.props.user.status === 'lost' ? 'J\'ai tout compris' :
+                                'J\'ai des difficultés'}
+                        placement="right">
+                    <Button
+                        variant="fab"
+                        color={this.props.user.status === 'lost' ? 'primary' : 'secondary'}
+                        style={styles.stopButton}
+                        onClick={this.onPressButton}>
+                        {this.props.user.status === 'lost' ? <OkIcon style={styles.stopIcon}/> :
+                            <StopIcon style={styles.stopIcon}/>}
+                    </Button>
+                    </Tooltip>
+                    <div className={this.props.classes.statusText}>
+                        <Typography
+                            className={this.props.classes.statusLabelText}
+                            variant="subheading">
+                            Statut actuel :
+                        </Typography>
+                        <Typography
+                            variant="button"
+                            color={this.props.user.status === 'lost' ? 'secondary' : 'primary'}
+                            className={this.props.classes.statusActualText}>
+                            {this.props.user.status === 'lost' ? <span>J'ai des difficultés</span> :
+                                <span>J'ai tout compris</span>}
+                        </Typography>
+                    </div>
+                </div>
+
+                <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
+                    <Typography
+                        className={this.props.classes.infoText}
+                        variant="subheading">
+                        <InfoIcon style={styles.infoIcon}/>
+                        Appuyer sur le bouton permet de notifer au professeur que vous avez du mal à suivre ou non
+                    </Typography>
+                </div>
+
             </div>
-
-            <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
-                <Button
-                    variant="fab"
-                    color={this.props.user.status === 'lost' ? 'primary' : 'secondary'}
-                    style={styles.stopButton}
-                    onClick={this.onPressButton}>
-                    {this.props.user.status === 'lost' ? <OkIcon style={styles.stopIcon}/> :
-                        <StopIcon style={styles.stopIcon}/>}
-                </Button>
-                <Typography
-                    variant="button"
-                    color={this.props.user.status === 'lost' ? 'primary' : 'secondary'}
-                    className={this.props.classes.stopButtonText}>
-                    {this.props.user.status === 'lost' ? <span>J'ai tout compris</span> :
-                        <span>J'ai des difficultés</span>}
-                </Typography>
-            </div>
-
-            <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
-                <Typography
-                    className={this.props.classes.infoText}
-                    variant="subheading">
-                    <InfoIcon style={styles.infoIcon}/>
-                    Appuyer sur le bouton permet de notifer au professeur que vous avez du mal à suivre
-                </Typography>
-            </div>
-
-        </div>
-    )
-  }
+        )
+    }
 }
 
 export default withStyles(styles)(StatusViewStudent);
--- frontend/src/components/StatusViewTeacher.js
+++ frontend/src/components/StatusViewTeacher.js
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
-import Button from 'material-ui/Button';
 import {withStyles} from 'material-ui';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
 import request from 'superagent';
 import PropTypes from 'prop-types';
 
@@ -8,88 +9,93 @@ import config from '../config';
 
 
 const styles = {
-  mainNumber: {
-    fontSize: 75,
-    margin: 20,
-  },
-  centerMe: {
-    textAlign: 'center',
-  },
-  resetButton: {
-    margin: 10,
-  },
+    mainNumber: {
+        fontSize: 75
+    },
+    centerMe: {
+        textAlign: 'center',
+    },
+    spaceMe: {
+        padding: 20
+    }
 };
 
 class StatusViewTeacher extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      totalStatus: [],
+    constructor(props) {
+        super(props);
+        this.state = {
+            totalStatus: [],
+        };
+    }
+
+    static propTypes = {
+        classes: PropTypes.object.isRequired,
+        user: PropTypes.object,
     };
-  }
 
-  static propTypes = {
-    classes: PropTypes.object.isRequired,
-    user: PropTypes.object,
-  };
+    componentDidMount() {
+        this.getTotalStatus();
+    }
 
-  componentDidMount() {
-    this.getTotalStatus();
-  }
+    async getTotalStatus() {
+        return request.get(`${config.remote.host}/api/users/status`)
+            .then(res => {
+                this.setState({
+                    totalStatus: res.body
+                });
+            })
+            .catch(err => {
+                console.error(err.message);
+            });
+    }
 
-  async getTotalStatus() {
-    return request.get(`${config.remote.host}/api/users/status`)
-      .then(res => {
-        this.setState({
-          totalStatus: res.body
-        });
-      })
-      .catch(err => {
-        console.error(err.message);
-      });
-  }
+    onPressResetButton = () => {
+        return request.get(`${config.remote.host}/api/users/resetAllStatus`)
+            .then(res => {
+                this.setState({
+                    totalStatus: res.body
+                });
+            })
+            .catch(err => {
+                console.error(err.message);
+            });
+    };
 
-  onPressResetButton = () => {
-    return request.get(`${config.remote.host}/api/users/resetAllStatus`)
-      .then(res => {
-        this.setState({
-          totalStatus: res.body
-        });
-      })
-      .catch(err => {
-        console.error(err.message);
-      });
-  };
+    render() {
+        return (
+            <div className={this.props.classes.centerMe}>
 
-  render() {
-    return (
-      <div className={this.props.classes.centerMe}>
+                <div className={this.props.classes.spaceMe}>
+                    <Typography variant="headline">
+                        Bienvenue {this.props.user.name}
+                    </Typography>
+                </div>
 
-        <div className={this.props.classes.centerMe}>
-          Bienvenue {this.props.user.name}
-        </div>
+                <div className={this.props.classes.spaceMe}>
+                    <span className={this.props.classes.mainNumber}>
+                        {this.state.totalStatus.reduce((acc, curVal) => {
+                            return acc + (curVal === 'lost' ? 1 : 0);
+                        }, 0)}
+                    </span>
+                    <Typography
+                        variant="subheading">
+                        <span>Élèves n'arrivent plus à suivre votre cours</span>
+                    </Typography>
+                </div>
 
-        <div className={this.props.classes.mainNumber}>
-          {this.state.totalStatus.reduce((acc, curVal) => {
-            return acc + (curVal === 'lost' ? 1 : 0);
-          }, 0)}
-        </div>
+                <div className={this.props.classes.spaceMe}>
+                    <Button
+                        variant="raised"
+                        color="primary"
+                        className={this.props.classes.resetButton}
+                        onClick={this.onPressResetButton}>
+                        Remettre à zero
+                    </Button>
+                </div>
 
-        <div>
-          Élèves n'arrivent plus à suivre votre cours
-        </div>
-
-        <Button
-          variant="flat"
-          color="primary"
-          className={this.props.classes.resetButton}
-          onClick={this.onPressResetButton}>
-          Remettre à zero
-        </Button>
-
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 export default withStyles(styles)(StatusViewTeacher);
