f2659f2706b369e4e0cc1d43401e65031f0de458
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2018-03-14 15:18:21 +0100

    redirect user to Linkapp on 401

--- backend/src/auth/auth.middleware.js
+++ backend/src/auth/auth.middleware.js
@@ -2,7 +2,7 @@ const config = require('../config');
 
 const isAuthenticated = (req, res, next) => {
   // TODO : Check token validity in Linkapp
-  if (!req.user) return res.status(401).send({ authUrl: config.authUrl });
+  if (!req.user) return res.status(401).json({ authUrl: config.authUrl });
   return next();
 };
 
--- frontend/src/repository/users.repository.js
+++ frontend/src/repository/users.repository.js
@@ -7,7 +7,7 @@ export async function resetAllStatus() {
   return new Promise((resolve, reject) => {
     request.get(`${config.remote.host}/api/users/resetAllStatus`)
       .then(res => resolve(res.body))
-      .catch(err => reject(err));
+      .catch(err => handleError(err));
   })
 }
 
@@ -15,7 +15,7 @@ export async function getTotalStatus() {
   return new Promise((resolve, reject) => {
     request.get(`${config.remote.host}/api/users/status`)
       .then(res => resolve(res.body))
-      .catch(err => reject(err));
+      .catch(err => handleError(err));
   })
 }
 
@@ -23,7 +23,7 @@ export async function getUser(userId) {
   return new Promise((resolve, reject) => {
     request.get(`${config.remote.host}/api/users/${userId}`)
       .then(res => resolve(res.body))
-      .catch(err => reject(err));
+      .catch(err => handleError(err));
   })
 }
 
@@ -31,6 +31,16 @@ export async function saveUserStatus(userId, status) {
   return new Promise((resolve, reject) => {
     request.put(`${config.remote.host}/api/users/${userId}/changeStatus`)
       .send({ status: status })
-      .catch(err => reject(err));
+      .catch(err => handleError(err));
   })
 }
+
+const handleError = (err) => {
+  if (err.status === 401) {
+    const authUrl = err.response.body.authUrl;
+    window.location.replace(
+      `${authUrl}?redirect=${encodeURIComponent(document.location.href)}`
+    );
+  }
+  return Promise.reject(err);
+};
