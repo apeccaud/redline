f099f3ae1cfb9fab4954e0f68af875d36dff6dde
Lucas Louf <lucaslouf@gmail.com>
2018-03-22 00:07:40 +0100

    Create QCMCreateViewTeacher

---
+++ frontend/src/components/QCMCreateViewTeacher.js
@@ -0,0 +1,150 @@
+import React, {PureComponent} from 'react';
+import {withStyles} from 'material-ui';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import {FormControl} from 'material-ui/Form';
+import { Clear as WrongIcon, Done as RightIcon } from 'material-ui-icons';
+import Input from 'material-ui/Input';
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+
+const styles = {
+  centerMe: {
+    textAlign: 'center',
+  },
+  spaceMe: {
+    padding: 20
+  },
+  flexOne: {
+    flex: 1,
+  },
+  questionBox: {
+    margin: 'auto',
+    maxWidth: 800,
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  answerInputForm: {
+    padding: '5px 0px',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  answerInput: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  questionLabelText: {
+    marginBottom: 10,
+  },
+  questionInput: {
+    border: '1px solid lightgrey',
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
+  LaunchQuestionButton: {
+    marginTop: 20,
+  }
+};
+
+class QCMCreateViewTeacher extends PureComponent {
+
+  render() {
+
+    return (
+      <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe + ' ' + this.props.classes.flexOne}>
+
+        <Paper className={this.props.classes.questionBox}>
+
+          <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
+            <Typography variant="headline">
+              Créer une question
+            </Typography>
+          </div>
+
+          <div className={this.props.classes.centerMe + ' ' + this.props.classes.spaceMe}>
+            <form noValidate autoComplete="off">
+
+              <FormControl fullWidth>
+                <Typography
+                  className={this.props.classes.questionLabelText}
+                  variant="subheading"
+                  align="left">
+                  Intitulé de la question
+                </Typography>
+                <Input
+                  id="question"
+                  type="text"
+                  onChange=""
+                  multiline="true"
+                  rowsMax="3"
+                  autoFocus="true"
+                  className={this.props.classes.questionInput}
+                />
+              </FormControl>
+
+              <FormControl className={this.props.classes.answerInputForm}>
+                <RightIcon/>
+                <Input
+                  id="right-answer"
+                  type="text"
+                  className={this.props.classes.answerInput}
+                  disableUnderline="true"
+                  placeholder="Bonne réponse"
+                />
+              </FormControl>
+              <Divider />
+              <FormControl className={this.props.classes.answerInputForm}>
+                <WrongIcon/>
+                <Input
+                  id="wrong-answer"
+                  type="text"
+                  className={this.props.classes.answerInput}
+                  disableUnderline="true"
+                  placeholder="Mauvaise réponse"
+                />
+              </FormControl>
+              <Divider />
+              <FormControl className={this.props.classes.answerInputForm}>
+                <WrongIcon/>
+                <Input
+                  id="wrong-answer"
+                  type="text"
+                  className={this.props.classes.answerInput}
+                  disableUnderline="true"
+                  placeholder="Mauvaise réponse"
+                />
+              </FormControl>
+              <Divider />
+              <FormControl className={this.props.classes.answerInputForm}>
+                <WrongIcon/>
+                <Input
+                  id="wrong-answer"
+                  type="text"
+                  className={this.props.classes.answerInput}
+                  disableUnderline="true"
+                  placeholder="Mauvaise réponse"
+                />
+              </FormControl>
+
+              <Button
+                variant="raised"
+                color="primary"
+                size="large"
+                className={this.props.classes.LaunchQuestionButton}>
+                Lancer la question
+              </Button>
+
+            </form>
+          </div>
+
+        </Paper>
+
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(QCMCreateViewTeacher);
--- frontend/src/components/StatusViewTeacher.js
+++ frontend/src/components/StatusViewTeacher.js
@@ -7,6 +7,7 @@ import { connect } from 'react-redux';
 import socket from '../services/sockets';
 import { resetAllStatus as resetAllStatusRep, getTotalStatus as getTotalStatusRep } from "../repository/users.repository";
 
+import QCMCreateViewTeacher from './QCMCreateViewTeacher';
 
 const styles = {
   mainNumber: {
@@ -17,6 +18,12 @@ const styles = {
   },
   spaceMe: {
     padding: 20
+  },
+  flexOne: {
+    flex: 1,
+  },
+  mainBox: {
+    display: 'flex',
   }
 };
 
@@ -62,32 +69,37 @@ class StatusViewTeacher extends PureComponent {
 
   render() {
     return (
-      <div className={this.props.classes.centerMe}>
+      <div className={this.props.classes.mainBox}>
 
-        <div className={this.props.classes.spaceMe}>
-          <Typography variant="headline">
-            Bienvenue {this.props.user.name}
-          </Typography>
+        <div className={this.props.classes.centerMe + ' ' + this.props.classes.flexOne}>
+
+          <div className={this.props.classes.spaceMe}>
+            <Typography variant="headline">
+              Bienvenue {this.props.user.name}
+            </Typography>
+          </div>
+
+          <div className={this.props.classes.spaceMe}>
+            <span className={this.props.classes.mainNumber}>
+              {this.getSumLostStatus()}
+            </span>
+            <Typography variant="subheading">
+              <span>Élèves n'arrivent plus à suivre votre cours</span>
+            </Typography>
+          </div>
+
+          <div className={this.props.classes.spaceMe}>
+            <Button
+              variant="raised"
+              color="primary"
+              className={this.props.classes.resetButton}
+              onClick={this.onPressResetButton}>
+              Remettre à zero
+            </Button>
+          </div>
         </div>
 
-        <div className={this.props.classes.spaceMe}>
-          <span className={this.props.classes.mainNumber}>
-            {this.getSumLostStatus()}
-          </span>
-          <Typography variant="subheading">
-            <span>Élèves n'arrivent plus à suivre votre cours</span>
-          </Typography>
-        </div>
-
-        <div className={this.props.classes.spaceMe}>
-          <Button
-            variant="raised"
-            color="primary"
-            className={this.props.classes.resetButton}
-            onClick={this.onPressResetButton}>
-            Remettre à zero
-          </Button>
-        </div>
+        <QCMCreateViewTeacher/>
 
       </div>
     )
