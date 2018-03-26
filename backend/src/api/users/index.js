1bd32c81c136abd6cd910dc76bd2883635fc9693
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2018-03-26 15:55:07 +0200

    Fix apidoc @apigroup

--- backend/src/api/users/index.js
+++ backend/src/api/users/index.js
@@ -6,7 +6,7 @@ const controler = require('./users.controller');
 /**
  * @api {get} /users Get all users
  * @apiName GetAllUsers
- * @apiGroup Static Pages
+ * @apiGroup Users
  * @apiDescription Affiche tous les utilisateurs
  */
 router.get('/', controler.findAll);
@@ -14,7 +14,7 @@ router.get('/', controler.findAll);
 /**
  * @api {post} /users Create a user
  * @apiName CreateUser
- * @apiGroup Static Pages
+ * @apiGroup Users
  * @apiDescription Crée un utilisateur
  *
  * @apiParam  {String} name Nom de l'user à créer
@@ -25,7 +25,7 @@ router.post('/', controler.create);
 /**
  * @api {get} /users/status Get all user status
  * @apiName GetAllUserStatus
- * @apiGroup Static Pages
+ * @apiGroup Users
  * @apiDescription Permet d'obtenir une liste des status de tous les utilisateurs
  */
 router.get('/status', controler.getAllStatus);
@@ -33,7 +33,7 @@ router.get('/status', controler.getAllStatus);
 /**
  * @api {get} /users/resetAllStatus Reset status of all users to 'neutral'
  * @apiName resetAllStatus
- * @apiGroup Static Pages
+ * @apiGroup Users
  * @apiDescription Réinitialise le statut de tous les utilisateurs à 'neutral'
  */
 router.get('/resetAllStatus', controler.resetAllStatus);
@@ -41,7 +41,7 @@ router.get('/resetAllStatus', controler.resetAllStatus);
 /**
  * @api {get} /users/get Get user in request parameter
  * @apiName getFromJWT
- * @apiGroup Static Pages
+ * @apiGroup Users
  * @apiDescription Renvoie l'utilisateur dans le paramètre req
  */
 router.get('/get', controler.getUser);
@@ -49,7 +49,7 @@ router.get('/get', controler.getUser);
 /**
  * @api {get} /users/:id Get one user
  * @apiName GetUser
- * @apiGroup Static Pages
+ * @apiGroup Users
  * @apiDescription Affiche un utilisateur à partir de son id
  *
  * @apiParam  {String} id id de l'user à récupérer
@@ -59,7 +59,7 @@ router.get('/:id', controler.findOne);
 /**
  * @api {get} /users/:id/status Get status of a user
  * @apiName GetUserStatus
- * @apiGroup Static Pages
+ * @apiGroup Users
  * @apiDescription Affiche le statut d'un utilisateur à partir de son id
  *
  * @apiParam  {String} id id de l'user à récupérer
@@ -69,7 +69,7 @@ router.get('/:id/status', controler.getStatus);
 /**
  * @api {put} /users/:id/changeStatus Change status of a user
  * @apiName ChangeUserStatus
- * @apiGroup Static Pages
+ * @apiGroup Users
  * @apiDescription Change le statut d'un utilisateur par un statut spécifié et respectant la
  * validation ('lost' ou 'neutral')
  *
